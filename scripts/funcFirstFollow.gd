b07a08d60edfb56bc7f517b5fbd19177c9ef4b83
Cristian Morataya <cris.morataya@gmail.com>
2018-05-31 11:54:07 -0600

    Add quit action in ESC key; Add return to previous scene button; Test dictionary content

--- scripts/funcFirstFollow.gd
+++ scripts/funcFirstFollow.gd
@@ -6,3 +6,14 @@ func _ready():
 
 	var label = get_node("New Scene")
 	label.set_text(dictionary.to_json())
+	print((dictionary.keys()))
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): get_tree().quit()
+
+func first():
+	pass
+
+func _on_btn_return_pressed():
+	get_tree().change_scene("res://scenes/main.tscn")
