512d033017d2f9e8d7b1f48e24e2a8695fbc4886
Cristian Morataya <cris.morataya@gmail.com>
2018-05-31 21:51:11 -0600

    Success building the function First

--- scripts/funcFirstFollow.gd
+++ scripts/funcFirstFollow.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
+var printString = ""
+
 func _ready():
 	var dictionary = global.productionDictionary
-	#print(dictionary)
 
 	var label = get_node("New Scene")
 	label.set_text(dictionary.to_json())
 	var keys = dictionary.keys()
 	for key in keys:
 		first_set(key, dictionary)
+	print(printString)
 	set_process_input(true)
 
 func _input(event):
@@ -18,20 +20,25 @@ func first_set(key, dictionary):
 	var first = ""
 	var evaluatedProduction = dictionary[key]
 	for item in evaluatedProduction:
-		var splittedItem = Array(item.split(" "))
-		if splittedItem.size() > 0:
-			var index = 0
-			while index != -1:
-				index = splittedItem.find("")
-				splittedItem.remove(index)
-			print(splittedItem)
-			# CM: Eval rule No.1 OR nule No.3
-			if global.terminals.has(splittedItem[0]) || splittedItem[0] == str("e"):
-				first += str(splittedItem[0], " ")
-			# CM: Eval rule No.2
-			else:
-				first_set(splittedItem[0], dictionary)
-	print(str("Primero(", key, ")\t->\t ", first))
+		if (typeof(item) == TYPE_STRING):
+			var splittedItem = Array(item.split(" "))
+			if splittedItem.size() > 0:
+				var index = 0
+				while index != -1:
+					index = splittedItem.find("")
+					splittedItem.remove(index)
+				# CM: Eval rule No.1 OR nule No.3
+				if global.terminals.has(splittedItem[0]) || splittedItem[0] == str("e"):
+					first += str(splittedItem[0], " , ")
+				# CM: Eval rule No.2
+				else:
+					printString += str("Primero(", key, ")\t->\t ")
+					first_set(splittedItem[0], dictionary)
+		elif (typeof(item) == TYPE_STRING_ARRAY):
+			# TODO-CM: handle StringArray to String
+			pass
+	if first != "":
+		printString += str("Primero(", key, ")\t->  {  ", first.substr(0, first.length()-3), "  }\n")
 
 func follow_set():
 	pass
